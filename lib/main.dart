f7f81213bf8af7dbe2ce8c4e79412181442e5915
Saransh29 <saranshbibiyan234@gmail.com>
2022-06-09 01:59:06 +0530

    v3 -added chart bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:expenses_tracker/widgets/chart.dart';
+
 import './widgets/transaction_list.dart';
 import './widgets/new_transactions.dart';
 import 'package:flutter/material.dart';
@@ -29,19 +31,27 @@ class _MyHomePageState extends State<MyHomePage> {
   // String titleInput;
 
   final List<Transaction> _userTransactions = [
-    Transaction(
-      id: 't1',
-      title: 'Item 1',
-      amount: 123.21,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Item 2',
-      amount: 23.31,
-      date: DateTime.now(),
-    ),
+    // Transaction(
+    //   id: 't1',
+    //   title: 'Item 1',
+    //   amount: 123.21,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Item 2',
+    //   amount: 23.31,
+    //   date: DateTime.now(),
+    // ),
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(DateTime.now().subtract(
+        Duration(days: 7),
+      ));
+    }).toList();
+  }
 
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
@@ -92,16 +102,16 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              height: 150,
-              color: const Color.fromARGB(255, 114, 178, 231),
-              // width: double.infinity,
-              child: const Text(
-                '<ADD CHART HERE>!',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 40, fontStyle: FontStyle.italic),
-              ),
-            ),
+            // Container(
+            //   color: const Color.fromARGB(255, 114, 178, 231),
+            //   // width: double.infinity,
+            //   child: const Text(
+            //     '',
+            //     textAlign: TextAlign.center,
+            //     style: TextStyle(fontSize: 40, fontStyle: FontStyle.italic),
+            //   ),
+            // ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions),
           ],
         ),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,53 +10,76 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 450,
-      child: ListView(
-        children: transactions.map((tx) {
-          return Card(
-            child: Row(
+      child: transactions.isEmpty
+          ? Column(
+              // ignore: prefer_const_literals_to_create_immutables
               children: <Widget>[
+                const Text(
+                  'No transactions added yet',
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 0, 57, 45),
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
                 Container(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    color: Color.fromARGB(255, 189, 255, 254),
-                  ),
-                  padding: const EdgeInsets.all(15),
-                  child: Text(
-                    '\$${tx.amount.toStringAsFixed(2)}', //tx.amount.toString() can also be used string interpolation
-                    style: const TextStyle(
-                      fontSize: 13,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      tx.title,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(255, 14, 73, 124),
-                      ),
-                    ),
-                    Text(
-                      DateFormat.yMMMd().format(tx.date),
-                      style: const TextStyle(
-                        fontSize: 15,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                )
               ],
+            )
+          : ListView(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                            vertical: 15, horizontal: 30),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Color.fromARGB(255, 189, 255, 254),
+                        ),
+                        padding: const EdgeInsets.all(15),
+                        child: Text(
+                          '\$${tx.amount.toStringAsFixed(2)}', //tx.amount.toString() can also be used string interpolation
+                          style: const TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tx.title,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 14, 73, 124),
+                            ),
+                          ),
+                          Text(
+                            DateFormat.yMMMd().format(tx.date),
+                            style: const TextStyle(
+                              fontSize: 15,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
             ),
-          );
-        }).toList(),
-      ),
     );
   }
 }
